389a7c83bdf308d09928f5ad378bcca338656d9a
wang1128 <penghao@pal-nat184-000-165.itap.purdue.edu>
2015-09-13 18:31:16 -0400

    Something still wrong

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,16 +1,26 @@
 __author__ = 'Penghao Wang'
 from numpy import *
 from math import *
-import csv
+
 #load data in traindata
 #load label in the trainlabel
 traindata = loadtxt('training.csv',usecols=range(9),dtype=int)
 trainlabel = loadtxt('training.csv',usecols=(range(9,10)),dtype=int)
-d=traindata[0]
+validatingdata = loadtxt('validating.csv',usecols=range(9),dtype=int)
+validatingLabel = loadtxt('validating.csv',usecols=(range(9,10)),dtype=int)
+testdata = loadtxt('testing.csv',usecols=range(9),dtype=int)
+testLabel = loadtxt('testing.csv',usecols=(range(9,10)),dtype=int)
+feaname = ["#0","#1","#2","#3","#4","#5","#6","#7","#8"]
 
-#print(d[0])
-#print(trainlabel)
+args = mean(traindata,axis = 0)
 
+class Node:
+    count=0
+    nodeList=set()
+    nodeAttributeidx=[0,1,2,3,4,5,6,7,8]
+    nodeAidxSplitValue=[0,0,0,0,0,0,0,0,0]
+
+node= Node()
 
 #calculate entropy
 def calculateEntropy(label):
@@ -28,26 +38,11 @@ def calculateEntropy(label):
 
     return entropy
 
-x=calculateEntropy(trainlabel)
-print(x)
-
-m= [label for label in trainlabel]
-featValues = [datasets[8] for datasets in traindata]
-uniqueFeatValues = set(featValues)
-mylist = list(uniqueFeatValues)
-#print(len(featValues))
-#print(featValues)
-print(mylist[1])
-
-
-
 #split the dataset 
 def splitdata(data,splitfeature_index,splitvalue):
     index_less = []
     index_more = []
 
-    #arg = mean(data[splitfeature_index],axis=0) # I use arg here because I will calculate imformation gain of less or greater than the average
-    #print(arg)
     for index in range(len(data)):
         d = data[index]
         if d[splitfeature_index] < splitvalue:
@@ -55,60 +50,163 @@ def splitdata(data,splitfeature_index,splitvalue):
         else:
             index_more.append(index)
     return index_less,index_more
-idx_less,idx_greater = splitdata(traindata,0,5)
-#print(idx_less)
-#print(idx_greater)
-#select the best data feature to split the datasets by using information gain
 
-def chooseSplitNode(data,label):
-    n_fea = len(data[0])
+def chooseBestSplitNode(data,label):
+    best_featureindex=-1
+    splitFeatures=-1
+    classList = [example for example in trainlabel]
 
-    n = len(label)
-    #print(data)
-    base_entropy = calculateEntropy(label)
-    best_gain = 0
-    for fea_i in range(n_fea): #calculate entropy under each splitting feature
-        cur_entropy = 0
-        featValues = [datasets[fea_i] for datasets in traindata]
-        uniqueFeatValues = list(set(featValues))
-        print(uniqueFeatValues)
-        #print(uniqueFeatValues)
-        for i in uniqueFeatValues:
+    if size(data) not in range(0,2) and classList.count(classList[0]) != len(classList) :
+        n_fea = len(data[0])
+        n = len(label)
+        base_entropy = calculateEntropy(label)
+        best_gain = 0
+        for fea_i in node.nodeAttributeidx:
             cur_entropy = 0
-            idxset_less,idxset_greater = splitdata(data,fea_i,i)
-            #print(idxset_less)
-            prob_less = float(len(idxset_less))/n
-            prob_greater = float(len(idxset_greater))/n
+            featValues = [datasets[fea_i] for datasets in traindata]
+            uniqueFeatValues = list(set(featValues))
 
-        #entropy(value|X) = \sum{p(xi)*entropy(value|X=xi)}
-            cur_entropy += prob_less*calculateEntropy(label[idxset_less])
-            cur_entropy += prob_greater * calculateEntropy(label[idxset_greater])
+            for i in uniqueFeatValues:
+                cur_entropy = 0
+                idxset_less,idxset_greater = splitdata(data,fea_i,i)
 
-            info_gain = base_entropy - cur_entropy #notice gain is before minus after
-            print (info_gain)
-            if(info_gain>best_gain):
-                best_gain = info_gain
-                best_idx = fea_i
-                splitFeatures = i
-    print(best_gain)
-    print(best_idx)
-    print(splitFeatures)
-    return best_idx,splitFeatures
+                prob_less = float(len(idxset_less))/n
+                prob_greater = float(len(idxset_greater))/n
+                cur_entropy += prob_less*calculateEntropy(label[idxset_less])
+                cur_entropy += prob_greater * calculateEntropy(label[idxset_greater])
+                info_gain = base_entropy - cur_entropy
 
+                if(info_gain>best_gain):
+                    best_gain = info_gain
+                    best_featureindex = fea_i
+                    splitFeatures = i
 
+    return best_featureindex,splitFeatures
 
 
-chooseSplitNode(traindata,trainlabel)
+#print(secondLeftData)
+#print(len(secondLeftData))
+
+def buildTree(traindata,trainlabel):
+
+    if trainlabel.size <= 1:
+        return trainlabel[0]
+    classList = [example for example in trainlabel]
+    # the type is the same, so stop classify
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+
+    curbestixd,cursplitF=chooseBestSplitNode(traindata,trainlabel)
+    if curbestixd in node.nodeAttributeidx:
+        node.nodeAttributeidx.remove(curbestixd)
+    curnode= (curbestixd,cursplitF)
+    node.nodeAidxSplitValue[curbestixd]= cursplitF
+    if curnode == (-1,-1): return None
+    else:
+        print(curbestixd,cursplitF)
+        cur_feaname = feaname[curbestixd]
+        print cur_feaname
+        nodedict = {cur_feaname:{}}
+
+        node.count += 1
+        if curnode in node.nodeList:
+            return
+        f = (curnode,node.count)
+        node.nodeList.add(f)
+
+        curLeftIdx,curRightIdx=splitdata(traindata,curbestixd,cursplitF)
+        curLeftdataSet= traindata[curLeftIdx]
+        curLeftdataLabel = trainlabel[curLeftIdx]
+        curRightdataSet = traindata[curRightIdx]
+        curRightdataLabel = trainlabel[curRightIdx]
+    nodedict[cur_feaname]["leftNode"] = buildTree(curLeftdataSet,curLeftdataLabel)
+    print("l")
+    nodedict[cur_feaname]["RightNode"] = buildTree(curRightdataSet,curRightdataLabel)
+    print("r")
+    return nodedict
 
 
 
-#m= [label for label in trainlabel]
-#featValues = [datasets[6] for datasets in traindata]
-#uniqueFeatValues = set(featValues)
-#print(len(featValues))
-#print(featValues)
+def classify(mytree,testdata):
+    if type(mytree).__name__ != 'dict':
+        return mytree
+    fea_name = mytree.keys()[0] #get the name of first feature
+    fea_idx = feaname.index(fea_name) #the index of feature 'fea_name'
+    val = testdata[fea_idx]
+    nextbranch = mytree[fea_name]
 
-#print(uniqueFeatValues)
+    #judge the current value > or < the pivot (average)
+    if val>node.nodeAidxSplitValue[fea_idx]:
+        nextbranch = nextbranch["RightNode"]
+    else:
+        nextbranch = nextbranch["leftNode"]
+    return classify(nextbranch,testdata)
 
 
 
+
+
+
+
+
+
+
+
+#testcode
+curfeatidx,curSplitFeat= chooseBestSplitNode(traindata,trainlabel)
+print(curfeatidx,curSplitFeat)
+firstLeftIdx,firstRightIdx=splitdata(traindata,curfeatidx,curSplitFeat)
+firstLeftData = traindata[firstLeftIdx]
+#print(trainlabel[firstLeftIdx])
+#print(trainlabel[firstRightIdx])
+#print(len(trainlabel[firstLeftIdx]))
+#print(len(trainlabel[firstRightIdx]))
+
+sedlfeatidx,sedlSplitFeat= chooseBestSplitNode(traindata[firstLeftIdx],trainlabel[firstLeftIdx])
+print(sedlfeatidx,sedlSplitFeat)
+secLeftIdx,secRIdx = splitdata(traindata[firstLeftIdx],sedlfeatidx,sedlSplitFeat)
+#print(len(secLeftIdx),len(secRIdx))
+#print(secLeftIdx,secRIdx)
+#node.nodeAttributeidx.remove(2)
+#del node.nodeAttributeidx[2]
+sedrfeatidx,sedrSplitFeat= chooseBestSplitNode(traindata[firstRightIdx],trainlabel[firstRightIdx])
+print(sedrfeatidx,sedrSplitFeat)
+#secondLeftData =firstLeftData[secLeftIdx]
+
+#buildTree(traindata,trainlabel)
+
+#node.nodeAttributeidx.remove(8)
+
+
+
+mytree=buildTree(traindata,trainlabel)
+print(mytree)
+print(node.nodeList)
+a=0
+b=0
+c=0
+for idx in range(60):
+    tt = traindata[idx]
+    x = classify(mytree,tt)
+    if (x == trainlabel[idx]):
+        a+=1
+
+print a
+
+for idx in range(20):
+    tt = validatingdata[idx]
+    x = classify(mytree,tt)
+    if (x == validatingLabel[idx]):
+        b+=1
+
+print b
+
+
+
+for idx in range(20):
+    tt = testdata[idx]
+    x = classify(mytree,tt)
+    if (x == testLabel[idx]):
+        c+=1
+
+print c
---
+++ test.py
@@ -0,0 +1,6 @@
+__author__ = 'penghao'
+
+nodeAttributeidx=[0,1,2,3,4,5,6,7,8]
+del nodeAttributeidx[0]
+print(nodeAttributeidx)
+print(range(9))
