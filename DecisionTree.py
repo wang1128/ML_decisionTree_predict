2dc8b66da0167a95f35a0fcc47ecace6eb89a58d
wang1128 <penghao@pal-nat184-024-144.itap.purdue.edu>
2015-09-14 14:39:05 -0400

    1

--- DecisionTree.py
+++ DecisionTree.py
@@ -15,7 +15,7 @@ feaname = ["#0","#1","#2","#3","#4","#5","#6","#7","#8"]
 class Node:
     count=0
     nodeList=set()
-    nodeAttributeidx=[0,1,2,3,4,5,6,7,8]
+    nodeAttributeIdx=[0,1,2,3,4,5,6,7,8] #defalut the Attribute Index
     nodeAidxSplitValue=[0,0,0,0,0,0,0,0,0]
 
 node= Node()
@@ -36,20 +36,24 @@ def calculateEntropy(label):
 
     return entropy
 
-#split the dataset 
+#split the dataset by using splitFeature and splitValue
+#return the index_less and index_more of data
 def splitdata(data,splitfeature_index,splitvalue):
     index_less = []
     index_more = []
 
     for index in range(len(data)):
         d = data[index]
+        #if the value in the attributes less than split value that was choosed then add to index_less
         if d[splitfeature_index] < splitvalue:
             index_less.append(index)
         else:
             index_more.append(index)
     return index_less,index_more
 
+#chooseBestSplitNode by using Information Gain
 def chooseBestSplitNode(data,label):
+    #defalut values
     best_featureindex=-1
     splitFeatures=-1
     classList = [example for example in trainlabel]
@@ -59,11 +63,10 @@ def chooseBestSplitNode(data,label):
         n = len(label)
         base_entropy = calculateEntropy(label)
         best_gain = 0
-        for fea_i in node.nodeAttributeidx:
+        for fea_i in node.nodeAttributeIdx:
             cur_entropy = 0
             featValues = [datasets[fea_i] for datasets in traindata]
             uniqueFeatValues = list(set(featValues))
-
             for i in uniqueFeatValues:
                 cur_entropy = 0
                 idxset_less,idxset_greater = splitdata(data,fea_i,i)
@@ -81,34 +84,31 @@ def chooseBestSplitNode(data,label):
 
     return best_featureindex,splitFeatures
 
-
-#print(secondLeftData)
-#print(len(secondLeftData))
+#build the Tree by using splitFeature and splitValue
 
 def buildTree(traindata,trainlabel):
-
+    #if there is only one value then return the value
     if trainlabel.size <= 1:
         return trainlabel[0]
     classList = [example for example in trainlabel]
-    # the type is the same, so stop classify
+    #check whether the whole labels are same or not
     if classList.count(classList[0]) == len(classList):
         return classList[0]
     curbestixd,cursplitF=chooseBestSplitNode(traindata,trainlabel)
-    if curbestixd in node.nodeAttributeidx:
-        node.nodeAttributeidx.remove(curbestixd)
-    curnode= (curbestixd,cursplitF)
+    if curbestixd in node.nodeAttributeIdx:
+        node.nodeAttributeIdx.remove(curbestixd)
+    curNode= (curbestixd,cursplitF)
     node.nodeAidxSplitValue[curbestixd]= cursplitF
-    if curnode == (-1,-1): return None
+    if curNode == (-1,-1): return None
     else:
-        #print(curbestixd,cursplitF)
         cur_feaname = feaname[curbestixd]
-        #print cur_feaname
-        nodedict = {cur_feaname:{}}
+        #use dictionary to save the Tree
+        nodeDict = {cur_feaname:{}}
 
         node.count += 1
-        if curnode in node.nodeList:
+        if curNode in node.nodeList:
             return
-        f = (curnode,node.count)
+        f = (curNode,node.count)
         node.nodeList.add(f)
 
         curLeftIdx,curRightIdx=splitdata(traindata,curbestixd,cursplitF)
@@ -116,74 +116,74 @@ def buildTree(traindata,trainlabel):
         curLeftdataLabel = trainlabel[curLeftIdx]
         curRightdataSet = traindata[curRightIdx]
         curRightdataLabel = trainlabel[curRightIdx]
-    nodedict[cur_feaname]["leftNode"] = buildTree(curLeftdataSet,curLeftdataLabel)
-    nodedict[cur_feaname]["RightNode"] = buildTree(curRightdataSet,curRightdataLabel)
-    return nodedict
+    nodeDict[cur_feaname]["leftNode"] = buildTree(curLeftdataSet,curLeftdataLabel)
+    nodeDict[cur_feaname]["RightNode"] = buildTree(curRightdataSet,curRightdataLabel)
+    return nodeDict
 
-def classify(mytree,testdata):
-    if type(mytree).__name__ != 'dict':
-        return mytree
-    fea_name = mytree.keys()[0]
+def classify(myTree,testdata):
+    if type(myTree).__name__ != 'dict':
+        return myTree
+    fea_name = myTree.keys()[0]
     fea_idx = feaname.index(fea_name)
     val = testdata[fea_idx]
-    nextbranch = mytree[fea_name]
+    nextNode = myTree[fea_name]
 
     if val>node.nodeAidxSplitValue[fea_idx]:
-        nextbranch = nextbranch["RightNode"]
+        nextNode = nextNode["RightNode"]
     else:
-        nextbranch = nextbranch["leftNode"]
-    return classify(nextbranch,testdata)
+        nextNode = nextNode["leftNode"]
+    return classify(nextNode,testdata)
 
-def prone (validatingdata,validatingLabel,mytree):
+def countAcc (data,label,myTree):
+    countRight = 0
+    for idx in range(20):
+        t = data[idx]
+        x = classify(myTree,t)
+        if (x == label[idx]):
+            countRight+=1
 
-    node.nodeAttributeidx = [0,1,2,3,4,5,6,7,8]
-    for idx in node.nodeAttributeidx:
-        node.nodeAttributeidx = [0,1,2,3,4,5,6,7,8]
-        node.nodeAttributeidx.remove(idx)
+    return countRight
+
+#prune the Tree by using the validating Data
+def prune (validatingdata,validatingLabel,myTree):
+
+    node.nodeAttributeIdx = [0,1,2,3,4,5,6,7,8]
+    maxRightNum= countAcc(validatingdata,validatingLabel,myTree)
+
+    pruneNodeIdx = 0
+    for nodeIdx in node.nodeAttributeIdx:
+        node.nodeAttributeIdx = [0,1,2,3,4,5,6,7,8]
+        node.nodeAttributeIdx.remove(nodeIdx)
         proneTree = buildTree(traindata,trainlabel)
         rightValueCount=0
         for idx in range(20):
 
             test = validatingdata[idx]
-            x = classify(mytree,test)
+            x = classify(myTree,test)
             if (x == validatingLabel[idx]):
                 rightValueCount+=1
+        if maxRightNum< rightValueCount:
+            maxRightNum = rightValueCount
+            pruneIdx = nodeIdx
+        else:
+            pruneIdx = -1
 
-        print rightValueCount
-    return
+    node.nodeAttributeIdx = [0,1,2,3,4,5,6,7,8]
+    #if there is no need to prune the tree then not
+    if pruneIdx != -1:
+        node.nodeAttributeIdx.remove(pruneIdx)
+    proneTree = buildTree(traindata,trainlabel)
+    return proneTree
 
+def main():
+    myTree=buildTree(traindata,trainlabel)
+    pruneTree = prune(validatingdata,validatingLabel,myTree)
+    testAccValue=countAcc(testdata,testLabel,pruneTree)
+    print(pruneTree)
+    print(testAccValue)
+    testAccRate = 0.00
+    testAccRate = testAccValue.__float__()/len(testLabel).__float__()
 
-mytree=buildTree(traindata,trainlabel)
+    print("The accuracy rate of the tree by using the test data is:",testAccRate )
 
-print(mytree)
-
-#print(node.nodeList)
-a=0
-b=0
-c=0
-for idx in range(60):
-    tt = traindata[idx]
-    x = classify(mytree,tt)
-    if (x == trainlabel[idx]):
-        a+=1
-
-print a
-
-for idx in range(20):
-    tt = validatingdata[idx]
-    x = classify(mytree,tt)
-    if (x == validatingLabel[idx]):
-        b+=1
-
-print b
-
-
-
-for idx in range(20):
-    tt = testdata[idx]
-    x = classify(mytree,tt)
-    if (x == testLabel[idx]):
-        c+=1
-
-print c
-prone(validatingdata,validatingLabel,mytree)
+main()
